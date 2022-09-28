b682cc3741aa83dec3848fa87240f271b8ab56c2
VivekParida <tejaswivivek23@gmail.com>
2022-09-29 02:33:59 +0530

    onboarding page, modal, slider, indicator, signUp

---
+++ lib/Screens/otp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OTPstate extends StatefulWidget {
+  const OTPstate({Key? key}) : super(key: key);
+
+  @override
+  State<OTPstate> createState() => _OTPstateState();
+}
+
+class _OTPstateState extends State<OTPstate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
